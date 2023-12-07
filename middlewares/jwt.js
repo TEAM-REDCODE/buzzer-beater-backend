18f74ad243ae66795a856047ae5cde28580c46c1
ksb-hub <gayun53a@korea.ac.kr>
2023-12-07 12:35:53 +0000

    fix: gen access & refresh jwt
    resolve confict
    
    fix: gen access & refresh jwt
    
    feat: add api signup
    
    
    Merge-request: BUZZER-BEATER-MR-21
    Merged-by: 석범 김 <gayun5313@gmail.com>

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,17 +1,26 @@
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
+require('dotenv').config({path: '../.env'})
 
 const generateToken = (user) => {
-    const token = jwt.sign({
-        nickname: user.nickname, email: user.email
-    }, process.env.JWT_SECRET,{
-        subject: "Buzzer-Beater jwtToken",
-        expiresIn: 60*60
+    const accessToken = jwt.sign({
+        nickname: user.nickname,
+        email: user.email
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        subject: "Buzzer-Beater AccessToken",
+        expiresIn: 60*60 // 1시간
+    });
+
+    const refreshToken = jwt.sign({
+        nickname: user.nickname,
+        email: user.email,
+    }, process.env.REFRESH_TOKEN_SECRET, {
+        subject: "Buzzer-Beater RefreshToken",
+        expiresIn: '20d', // 20일
     });
 
     console.log("JWT is successfully generated!")
 
-    return token
+    return { accessToken, refreshToken }
 }
 
 const verifyToken = (req, res, next) => {
