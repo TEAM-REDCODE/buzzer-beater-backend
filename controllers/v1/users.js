3bee967e6b0ac024d5abb90a8bdb4542759bff45
ksb-hub <gayun53a@korea.ac.kr>
2023-12-07 13:31:13 +0000

    feat: add api login
    Merge-request: BUZZER-BEATER-MR-22
    Merged-by: 석범 김 <gayun5313@gmail.com>

--- controllers/v1/users.js
+++ controllers/v1/users.js
@@ -33,11 +33,57 @@ router.post('/signup', async (req, res)=>{
         const newUserToken = generateToken(newUser)
 
         console.log("Data is created!")
-        res.status(201).json({message: 'User registration successful', token: newUserToken});
+        res.status(201).json({
+            message: 'User registration successful',
+            jwt: {
+                accessToken: newUserToken.accessToken,
+                refreshToken: newUserToken.refreshToken
+            }
+        });
+    } catch (error){
+        console.error(error)
+        if (error.name === 'SequelizeValidationError') {
+            res.status(400).json({ error: error.errors });
+        } else {
+            res.status(500).json({ error: 'Internal Sever Error' });
+        }
+    }
+});
+
+router.post('/login',  async (req, res) => {
+    const { email, password } = req.body
+
+    try{
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatch){
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+
+        const userToken = generateToken(user)
+
+        console.log('Login!')
+        res.status(200).json({
+            message: 'Login successful',
+            nickname: user.nickname,
+            jwt: {
+                accessToken: userToken.accessToken,
+                refreshToken: userToken.refreshToken
+            }
+        });
     } catch (error){
         console.error(error)
         res.status(500).json({error: 'Internal Sever Error'});
     }
-});
+})
 
 module.exports = router
